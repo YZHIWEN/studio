33993f5be255ae74082265b1824e9e09e71d35b9
YZHIWEN <1425353753@qq.com>
2020-11-15 01:29:47 +0800

    new evec

--- vps/brook.sh
+++ vps/brook.sh
@@ -764,27 +764,16 @@ Brook_Menu() {
 	fi
 }
 
-exev() {
-	check_sys
-	bk_port=$config_port
-	bk_passwd=$config_password
-	brook_new_ver="v20200701"  #默认使用v20200701版本，新版本之前试过通信有问题
-	echo "开始执行配置 端口：${config_port} 密码：${config_password} 版本：${brook_new_ver}"
-	if[[ -e ${brook_file} ]]; then # 已安装brook
-		Start_brook # start
-	elif 
-		Install_brook # install and start
-	
-}
-
-
-while getopts :l:p:v:e:m opt
-do
-  case "$opt" in
-  e) exev ;; # 执行入口
-  m) Brook_Menu ;; # 菜单
-  l) config_port=$OPTARG;; # 端口
-  p) config_password=$OPTARG ;; # 密码
-  *) echo "Unknown option: $opt" ;;
-  esac
-done
+# exec
+bk_port=$1
+bk_passwd=$2
+echo "端口：${bk_port} 密码：${bk_passwd}"
+read -s -n1 -p "按任意键继续 ... "
+brook_new_ver="v20200701"  #默认使用v20200701版本，新版本之前试过通信有问题
+check_sys
+echo "开始执行配置 端口：${config_port} 密码：${config_password} 版本：${brook_new_ver}"
+if [[ -e ${brook_file} ]]; then # 已安装brook
+	Start_brook # start
+else 
+	Install_brook # install and start
+fi
--- vps/brook_boost.sh
+++ vps/brook_boost.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# brook_url=xxx
-# wget -N --no-check-certificate brook_url && chmod +x brook.sh && bash brook.sh
-# bash brook.sh -l xxx -p xxx -e
+brook_url=https://raw.githubusercontent.com/YZHIWEN/studio/dev/vps/brook.sh
+wget -N --no-check-certificate brook_url && chmod +x brook.sh && bash brook.sh
+./brook.sh 2333 987654321 # ./brook.sh port password
